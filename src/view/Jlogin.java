b03c01a88d2aaac570e501987d7a512e5da3c04f
iamjonn <jonata10.jn@gmail.com>
2025-01-10 01:54:25 -0300

    implementado validacao no login

--- src/view/Jlogin.java
+++ src/view/Jlogin.java
@@ -6,16 +6,20 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JPasswordField;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Jlogin extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
+	private JTextField textFieldUsuario;
 	private JLabel lblNewLabel_1;
 	private JPasswordField passwordField;
 
@@ -54,26 +58,41 @@ public class Jlogin extends JFrame {
 		panel.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("USUARIO");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblNewLabel.setBounds(74, 58, 83, 14);
 		panel.add(lblNewLabel);
 		
-		textField = new JTextField();
-		textField.setBounds(74, 71, 205, 20);
-		panel.add(textField);
-		textField.setColumns(10);
+		textFieldUsuario = new JTextField();
+		textFieldUsuario.setBounds(74, 71, 205, 20);
+		panel.add(textFieldUsuario);
+		textFieldUsuario.setColumns(10);
 		
 		lblNewLabel_1 = new JLabel("SENHA");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblNewLabel_1.setBounds(74, 102, 46, 14);
 		panel.add(lblNewLabel_1);
 		
 		JButton btnNewButton = new JButton("ENTRAR");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(textFieldUsuario.getText() != null && 
+						!textFieldUsuario.getText().isEmpty() && 
+						passwordField.getText()!=null &&
+						!passwordField.getText().isEmpty()){ 
+					JOptionPane.showConfirmDialog(btnNewButton, "informaçoes validas");
+				}else {
+					JOptionPane.showConfirmDialog(btnNewButton, "Verifique as informaçoes", "Aviso", JOptionPane.WARNING_MESSAGE);
+				}
+			}
+		});
 		btnNewButton.setForeground(new Color(255, 255, 255));
 		btnNewButton.setBackground(new Color(0, 64, 64));
 		btnNewButton.setBounds(119, 176, 89, 23);
 		panel.add(btnNewButton);
 		
 		JLabel lblNewLabel_2 = new JLabel("BEM VINDO");
-		lblNewLabel_2.setBounds(142, 27, 66, 14);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 22));
+		lblNewLabel_2.setBounds(103, 14, 138, 33);
 		panel.add(lblNewLabel_2);
 		
 		passwordField = new JPasswordField();
