e4c07ac4dba515a59e5a30febc57c1907cf0042f
iamjonn <jonata10.jn@gmail.com>
2025-01-10 00:56:47 -0300

    componentes tela login

--- src/view/Jlogin.java
+++ src/view/Jlogin.java
@@ -5,11 +5,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+import java.awt.Color;
 
 public class Jlogin extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JLabel lblNewLabel_1;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -34,9 +42,41 @@ public class Jlogin extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 64, 64));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(47, 11, 343, 222);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("USUARIO");
+		lblNewLabel.setBounds(74, 58, 83, 14);
+		panel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(74, 71, 205, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		lblNewLabel_1 = new JLabel("SENHA");
+		lblNewLabel_1.setBounds(74, 102, 46, 14);
+		panel.add(lblNewLabel_1);
+		
+		JButton btnNewButton = new JButton("ENTRAR");
+		btnNewButton.setBackground(new Color(0, 64, 64));
+		btnNewButton.setBounds(119, 176, 89, 23);
+		panel.add(btnNewButton);
+		
+		JLabel lblNewLabel_2 = new JLabel("BEM VINDO");
+		lblNewLabel_2.setBounds(142, 27, 66, 14);
+		panel.add(lblNewLabel_2);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(73, 117, 206, 20);
+		panel.add(passwordField);
 	}
-
 }
