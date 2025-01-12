242d148d112763d675b971a4b553ec4d7e4838ce
iamjonn <jonata10.jn@gmail.com>
2025-01-12 00:50:03 -0300

    implementado classe de criptografia

--- src/controller/Criptografia.java
+++ src/controller/Criptografia.java
@@ -1,5 +1,62 @@
 package controller;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Criptografia {
+	public static final String SHA256 = "SHA-256";
+	public static final String MD5 = "MD5";
+	
+	protected String informacao;
+	protected String padrao;
+	
+	public Criptografia(String informacao, String padrao) {
+		super();
+		this.informacao = informacao;
+		this.padrao = padrao;
+	}
+
+	public String getInformacao() {
+		return informacao;
+	}
+
+	public void setInformaçao(String informaçao) {
+		this.informacao = informaçao;
+	}
+
+	public String getPadrao() {
+		return padrao;
+	}
+
+	public void setPadrao(String padrao) {
+		this.padrao = padrao;
+	}
+	
+	public String criptografia() {
+		String informacao = getInformacao();
+		
+		MessageDigest messageDigest;
+		StringBuilder hexString = null;
+		
+		try {
+			messageDigest = MessageDigest.getInstance(getPadrao());
+			byte[] hash = messageDigest.digest(
+					informacao.getBytes(StandardCharsets.UTF_8));
+					hexString = new StringBuilder(2* hash.length);
+					for(int i = 0; i < hash.length; i++) {
+						String hex = Integer.toHexString(0xff & hash[i]);
+						if(hex.length() ==1) {
+							hexString.append('0');
+						}
+						hexString.append(hex);
+					}
+					
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				return hexString.toString().toUpperCase();
+	}
 
 }
